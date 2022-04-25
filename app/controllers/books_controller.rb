46da53f635c45293d5cdd2b30b6931cf052850e5
EC2 Default User <ec2-user@ip-172-31-15-215.ec2.internal>
2022-04-25 06:10:17 +0000

    Finish-ex2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,24 +2,35 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @newbook = Book.new
+    @user = @book.user
   end
 
   def index
+    @book = Book.new
     @books = Book.all
+    @user = current_user
   end
 
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
+      @user = current_user
       render 'index'
     end
   end
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+    render "edit"
+    else
+    redirect_to books_path
+    end
   end
 
   def update
@@ -31,15 +42,15 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title,:body)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  belongs_to :books
+  has_many :books, dependent: :destroy
   has_one_attached :profile_image
 
-  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :name, presence: false, uniqueness: true,
+         length: { minimum: 2, maximum: 20 }
+
+         validates :introduction, presence: false, length: { maximum: 50 }
+
 
   
   
